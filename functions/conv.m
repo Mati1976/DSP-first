c702266e27a6861d6456740da27f3cff39f54c4d
Mati Rachamim <rachamim@kth.se>
2020-12-30 02:04:50 +0100

    wtite 1st version of the convolution function

--- functions/conv.m
+++ functions/conv.m
@@ -1,2 +1,13 @@
-Leng=length(a)+length(b)-1;
-br=b(1,3:-1:1)
+function res=conv(a , b);
+
+    Leng=length(a)+length(b)-1;
+    an=[zeros(1,length(b)-1) a];
+    br=b(1,length(b):-1:1);
+    bn=[br  zeros(1,length(a)-1)];
+    res=zeros(1,Leng);
+    for i=1:Leng
+        c=an.*bn;
+        res(1,i)=sum(c);
+        bn=[0 bn(1:1:Leng-1)];
+    end
+end
