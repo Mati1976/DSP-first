de0a909e049903b58fd979edf01d546b54898a5d
Mati Rachamim <rachamim@kth.se>
2020-12-30 21:49:03 +0100

    Add the IMdft.m function

---
+++ functions/IMdft.m
@@ -0,0 +1,15 @@
+function ResMat=IMdft(N)
+%This function calculate te Nth order Inverse discrete DFT transform matrix
+%The input for the function is the order of the matrix needed .
+
+    %calculating the basic exponent that appears in every matrix component
+    E=exp(i*2*pi/N);
+    %Setting up a null Matrix in the right size
+    ResMat=zeros(N,N);
+    %Calculating the values of the matrix by raising the exponential to the
+    %right order
+    for k=0:(N-1)
+        for n=0:(N-1)
+            ResMat(k+1,n+1)=E^(k*n);
+        end
+    end
